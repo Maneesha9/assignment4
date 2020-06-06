d965016fae6fed07052f8268199074adeedec8c2
Enter Name <maneeshakorla38@gmail.com>
2020-06-06 16:56:33 +0530

    dice

---
+++ tailhead.sh
@@ -0,0 +1,10 @@
+       function  Flip() {
+               echo $1
+       }
+       result=$(Flip $(($RANDOM%2)))
+         if [ $result -eq 1 ]
+               then
+                       echo "Heads"
+              else
+                      echo "Tails"
+              fi
