6aee485f3864adc80f1a481395179e4d96e800db
Enter Name <maneeshakorla38@gmail.com>
2020-06-06 16:55:20 +0530

    dice

---
+++ monthday.sh
@@ -0,0 +1,13 @@
+       echo "Enter the month: "
+       read month
+       echo "Enter the day: "
+       read day
+       if (( ($month == 3 && $day >=20 && $day<=31)
+          || ($month == 4 && $day >=1 && $day<=31)
+          || ($month == 5 && $day >=1 && $day<=31)
+          || ($month == 6 && $day >=1 && $day<=20) ))
+      then
+              echo "True"
+      else
+              echo "False"
+      fi
