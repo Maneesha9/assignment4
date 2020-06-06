6e0ca08e129a089b9249d87d3beeffb80ae8c1d3
Enter Name <maneeshakorla38@gmail.com>
2020-06-06 16:50:09 +0530

    dice

---
+++ leapyear.sh
@@ -0,0 +1,8 @@
+       echo "Enter the year: "
+       read year
+       if [ $(($year%100))!=0 ] && [ $(($year%4)) -eq 0 ] || [ $(($year%400)) -eq 0 ]
+       then
+       echo "leap Year"
+       else
+       echo "Not a leap year"
+       fi
